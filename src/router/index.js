380026a4c22affa075e579ba1f86f2aeefb22ea6
Kabeh <kabeh.hb@gmail.com>
2024-03-27 16:04:44 +0000

    fetch all documents as index and route to document page by id

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
-import Documents from '../views/Documents.vue'
+/** @format */
 
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Documents from "../views/Documents.vue";
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
@@ -18,7 +19,7 @@ const router = createRouter({
 			component: () => import("../views/Edit.vue"),
 		},
 		{
-			path: "/docs",
+			path: "/documents",
 			name: "Documents",
 			component: () => import("../views/Documents.vue"),
 		},
@@ -30,13 +31,17 @@ const router = createRouter({
 				{ path: "register", component: Register },
 			],
 		},
-
 		{
-			path: "/new-doc",
-			name: "NewDoc",
+			path: "/document",
+			name: "document",
 			component: () => import("../views/DocumentPage.vue"),
+		},
+		{
+			path: "/document/:id",
+			name: "documentById",
+			component: () => import("../views/DocumentPage.vue"),
+			props: true,
 		},
 	],
 });
-
-export default router
+export default router;
--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -10,268 +10,61 @@
 		<div class="-m-1.5 overflow-x-auto">
 			<div class="p-1.5 min-w-full inline-block align-middle">
 				<div class="overflow-hidden">
-					<table
-						class="min-w-full divide-y divide-gray-200 dark:divide-gray-700"
-					>
+					<table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
 						<thead>
 							<tr>
-								<th
-									scope="col"
-									class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-								>
+								<th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
 									Title
 								</th>
-								<th
-									scope="col"
-									class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-								>
+								<th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
 									Created by
 								</th>
-								<th
-									scope="col"
-									class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-								>
+								<th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
 									Team
 								</th>
-								<th
-									scope="col"
-									class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-								>
+								<th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
 									Created at
 								</th>
-								<th
-									scope="col"
-									class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
-								>
+								<th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">
 									Action
 								</th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr
-								class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800"
-							>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									Document title one
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-									John doe
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-										alt=""
-									/>
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									11 March 2024
-								</td>
-
-								<td
-									class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium"
-								>
-									<button
-										type="button"
-										class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-									>
-										Edit
-									</button>
-								</td>
-							</tr>
-							<tr
-								class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800"
-							>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									Document title one
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-									John doe
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-										alt=""
-									/>
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									11 March 2024
-								</td>
-
-								<td
-									class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium"
-								>
-									<button
-										type="button"
-										class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-									>
-										Edit
-									</button>
-								</td>
-							</tr>
-							<tr
-								class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800"
-							>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									Document title one
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-									John doe
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-										alt=""
-									/>
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									11 March 2024
-								</td>
-
-								<td
-									class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium"
-								>
-									<button
-										type="button"
-										class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-									>
-										Edit
-									</button>
-								</td>
-							</tr>
-							<tr
-								class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800"
-							>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									Document title one
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-									John doe
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-								>
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-
-									<img
-										class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-										src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-										alt=""
-									/>
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
-								>
-									11 March 2024
-								</td>
-
-								<td
-									class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium"
-								>
-									<button
-										type="button"
-										class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-									>
-										Edit
-									</button>
-								</td>
-							</tr>
+							<ul>
+								<li v-for="document of documents" :key="document.id">
+									<tr class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800">
+										<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+											<router-link :to="{ path: `/document/${document.id}` }"><button>{{ document.title }}</button></router-link>
+										</td>
+										<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+											<img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+												src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+												alt="" />
+											John doe
+										</td>
+										<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+											<img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+												src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+												alt="" />
+											<img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+												src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+												alt="" />
+											<img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+												src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+												alt="" />
+										</td>
+										<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+											{{ document.created_at }} </td>
+										<td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+											<button type="button"
+												class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+												Edit
+											</button>
+										</td>
+									</tr>
+								</li>
+							</ul>
 						</tbody>
 					</table>
 				</div>
@@ -279,3 +72,27 @@
 		</div>
 	</div>
 </template>
+
+
+<script>
+import axios from 'axios';
+export default {
+	name: "documents",
+	data() {
+		return {
+			documents: []
+		};
+	},
+	methods: {
+		async myDocs() {
+			const res = await axios.get(`http://localhost:8000/documents`);
+			console.log(res.data)
+			this.documents = res.data;
+			console.log(this.documents)
+		}
+	},
+	mounted() {
+		this.myDocs()
+	},
+};
+</script>
