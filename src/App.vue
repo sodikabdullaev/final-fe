da40f8f6095eda3088ae769077c6d0aaa7f2bd5a
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-04-02 11:13:18 +0100

    logged in user persists after refresh

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Nav from './components/Nav.vue'
+import { RouterLink, RouterView } from "vue-router";
+import Nav from "./components/Nav.vue";
+import { store } from "./store";
+
+store.isLoggedIn = localStorage.getItem("currentUser") !== null;
 </script>
 
 <template>
-	<div class="min-h-full">
-		<Nav />
+  <div class="min-h-full">
+    <Nav />
 
-		<main>
-			<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-				<RouterView />
-			</div>
-		</main>
-	</div>
+    <main>
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <RouterView />
+      </div>
+    </main>
+  </div>
 </template>
